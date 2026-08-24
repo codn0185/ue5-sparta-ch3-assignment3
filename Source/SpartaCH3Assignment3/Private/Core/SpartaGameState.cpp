0b775b615300288f20f802f59cc6c875f1fcb345
김채우 <codn0185@gmail.com>
2026-08-24 20:01:09 +0900

    feat: ASpartaGameState 클래스 스켈레톤 코드 작성

--- Source/SpartaCH3Assignment3/Private/Core/SpartaGameState.cpp
+++ Source/SpartaCH3Assignment3/Private/Core/SpartaGameState.cpp
@@ -1,1 +1,48 @@
 #include "Core/SpartaGameState.h"
+
+ASpartaGameState::ASpartaGameState()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Game Data
+	Score = 0;
+	SpawnedCoinCount = 0;
+	CollectedCointCount = 0;
+}
+
+void ASpartaGameState::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASpartaGameState::StartLevel()
+{
+}
+
+void ASpartaGameState::EndLevel()
+{
+}
+
+void ASpartaGameState::OnCoinCollected()
+{
+}
+
+void ASpartaGameState::OnTimeExpired()
+{
+}
+
+void ASpartaGameState::OnPlayerDead()
+{
+}
+
+void ASpartaGameState::HandleGameOver()
+{
+}
+
+void ASpartaGameState::HandleGameCleared()
+{
+}
+
+void ASpartaGameState::AddScore(int32 amount)
+{
+}
--- Source/SpartaCH3Assignment3/Public/Core/SpartaGameState.h
+++ Source/SpartaCH3Assignment3/Public/Core/SpartaGameState.h
@@ -10,4 +10,41 @@ UCLASS()
 class SPARTACH3ASSIGNMENT3_API ASpartaGameState : public AGameState
 {
 	GENERATED_BODY()
+
+  public:
+	ASpartaGameState();
+
+  protected:
+	// Life Cycle Methods
+	virtual void BeginPlay() override;
+	void StartLevel();
+	void EndLevel();
+
+  public:
+	// Event Methods
+	void OnCoinCollected();
+	void OnTimeExpired();
+	void OnPlayerDead();
+
+  protected:
+	// Handler Methods
+	void HandleGameOver();
+	void HandleGameCleared();
+
+  public:
+	// Modifier Methods
+	UFUNCTION(BlueprintCallable, Category = "Actions|Score")
+	void AddScore(int32 Amount);
+
+  protected:
+	// Game Data
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GameData|Score")
+	int32 Score;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GameData|Coin")
+	int32 SpawnedCoinCount;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GameData|Coin")
+	int32 CollectedCointCount;
+
+	UPROPERTY()
+	FTimerHandle LevelTimerHandle;
 };
