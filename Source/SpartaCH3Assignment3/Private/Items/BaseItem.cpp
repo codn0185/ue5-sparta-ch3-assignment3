1c95bd20728c09dd0d0485b8fee1d223d5edf209
김채우 <codn0185@gmail.com>
2026-08-24 20:28:24 +0900

    feat: ABaseItem 클래스 추가

---
+++ Source/SpartaCH3Assignment3/Private/Items/BaseItem.cpp
@@ -0,0 +1,37 @@
+#include "Items/BaseItem.h"
+
+#include "Components/SphereComponent.h"
+
+ABaseItem::ABaseItem()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Initialize Components
+	RootCollision = CreateDefaultSubobject<USphereComponent>(TEXT("Root Collision"));
+	SetRootComponent(RootCollision);
+	RootCollision->SetCollisionProfileName(TEXT("OverlapAllDynamic"));
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	StaticMesh->SetupAttachment(RootCollision);
+
+	// 오버랩 이벤트 바인딩
+	RootCollision->OnComponentBeginOverlap.AddDynamic(this, &ABaseItem::OnBeginOverlap);
+	RootCollision->OnComponentEndOverlap.AddDynamic(this, &ABaseItem::OnEndOverlap);
+}
+
+void ABaseItem::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void ABaseItem::OnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+void ABaseItem::Activate(AActor* Activator)
+{
+}
+
+FName ABaseItem::GetType() const
+{
+	return Type;
+}
---
+++ Source/SpartaCH3Assignment3/Public/Items/BaseItem.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/Actor.h"
+#include "Interfaces/InteractableInterface.h"
+
+#include "BaseItem.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class SPARTACH3ASSIGNMENT3_API ABaseItem : public AActor, public IInteractableInterface
+{
+	GENERATED_BODY()
+
+  public:
+	ABaseItem();
+
+  protected:
+	// Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USphereComponent* RootCollision;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* StaticMesh;
+
+	// Properties
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Properties")
+	FName Type;
+
+	// Implement "IInteractableInterface" Methods
+	virtual void OnBeginOverlap(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult) override;
+	virtual void OnEndOverlap(
+		UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex) override;
+	virtual void Activate(AActor* Activator) override;
+	virtual FName GetType() const override;
+};
