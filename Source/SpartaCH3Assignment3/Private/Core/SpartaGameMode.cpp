7494ce079334ad763b3ddbf799bbb6bf9359cd5e
김채우 <codn0185@gmail.com>
2026-08-24 17:49:35 +0900

    feat: ASpartaGameState 클래스 생성 및 ASpartaGameMode의 기본 GameState로 등록

--- Source/SpartaCH3Assignment3/Private/Core/SpartaGameMode.cpp
+++ Source/SpartaCH3Assignment3/Private/Core/SpartaGameMode.cpp
@@ -2,9 +2,11 @@
 
 #include "Characters/SpartaCharacter.h"
 #include "Characters/SpartaPlayerController.h"
+#include "Core/SpartaGameState.h"
 
 ASpartaGameMode::ASpartaGameMode()
 {
 	DefaultPawnClass = ASpartaCharacter::StaticClass();
 	PlayerControllerClass = ASpartaPlayerController::StaticClass();
+	GameStateClass = ASpartaGameState::StaticClass();
 }
---
+++ Source/SpartaCH3Assignment3/Public/Core/SpartaGameState.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/GameState.h"
+
+#include "SpartaGameState.generated.h"
+
+UCLASS()
+class SPARTACH3ASSIGNMENT3_API ASpartaGameState : public AGameState
+{
+	GENERATED_BODY()
+};
