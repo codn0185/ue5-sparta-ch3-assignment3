309725ab1836f2e5bfe47333b705da6167f93c71
김채우 <codn0185@gmail.com>
2026-08-24 20:28:10 +0900

    chore: 메서드 이름 수정

--- Source/SpartaCH3Assignment3/Public/Interfaces/InteractableInterface.h
+++ Source/SpartaCH3Assignment3/Public/Interfaces/InteractableInterface.h
@@ -17,9 +17,9 @@ class SPARTACH3ASSIGNMENT3_API IInteractableInterface
 	GENERATED_BODY()
 
   public:
-	// 플레이어가 아이템 범위 안에 들어올 때 호출
+	// 플레이어가 범위 안에 들어올 때 호출
 	UFUNCTION()
-	virtual void OnItemBeginOverlap(
+	virtual void OnBeginOverlap(
 		UPrimitiveComponent* OverlappedComp,
 		AActor* OtherActor,
 		UPrimitiveComponent* OtherComp,
@@ -28,18 +28,15 @@ class SPARTACH3ASSIGNMENT3_API IInteractableInterface
 		const FHitResult& SweepResult) = 0;
 	// 플레이어가 아이템 범위 밖에 나갈 때 호출
 	UFUNCTION()
-	virtual void OnItemEndOverlap(
+	virtual void OnEndOverlap(
 		UPrimitiveComponent* OverlappedComp,
 		AActor* OtherActor,
 		UPrimitiveComponent* OtherComp,
 		int32 OtherBodyIndex) = 0;
-	// 아이템 활성화 시 호출
+	// 활성화 시 호출
 	UFUNCTION()
-	virtual void ActivateItem(AActor* Activator) = 0;
+	virtual void Activate(AActor* Activator) = 0;
 	// 아이템 타입 반환
 	UFUNCTION()
-	virtual FName GetItemType() const = 0;
-	// 아이템 제거 시 호출
-	UFUNCTION()
-	virtual void DestroyItem() = 0;
+	virtual FName GetType() const = 0;
 };
