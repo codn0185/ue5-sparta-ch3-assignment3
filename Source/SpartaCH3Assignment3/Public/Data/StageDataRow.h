19585aa01642f0b1cf03c39f5a17d962b1ace85b
김채우 <codn0185@gmail.com>
2026-08-25 12:56:24 +0900

    feat: FStageDataRow 생성 - 각 스테이지의 정보를 담는 데이터테이블 행 구조체
    - 스테이지 이름
    - 스테이지 맵
    - 제한 시간
    - 아이템 소환 데이터테이블

---
+++ Source/SpartaCH3Assignment3/Public/Data/StageDataRow.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "StageDataRow.generated.h"
+
+USTRUCT(BlueprintType)
+struct FStageDataRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+  public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName StageName;  // 스테이지 이름
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UWorld> Level;  // 스테이지 맵
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float TimeLimit;  // 제한 시간
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UDataTable> ItemSpawnTable;  // 아이템 소환 테이블
+};
