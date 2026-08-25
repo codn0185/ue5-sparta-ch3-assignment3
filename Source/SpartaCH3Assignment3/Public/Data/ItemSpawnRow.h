ceb53de3e3db88243e6dd622e2b5c8ee1b42b289
김채우 <codn0185@gmail.com>
2026-08-25 15:09:00 +0900

    refactor: 아이템 클래스를 BaseItem에서 AActor로 수정

--- Source/SpartaCH3Assignment3/Public/Data/ItemSpawnRow.h
+++ Source/SpartaCH3Assignment3/Public/Data/ItemSpawnRow.h
@@ -15,7 +15,7 @@ struct FItemSpawnRow : public FTableRowBase
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FName ItemName;  // 아이템 이름
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TSubclassOf<ABaseItem> ItemClass;  // 아이템 클래스
+	TSubclassOf<AActor> ItemClass;  // 아이템 클래스
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float SpawnChance;  // 소환 확률
 };
