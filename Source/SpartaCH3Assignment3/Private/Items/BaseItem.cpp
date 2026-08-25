29ff1a6dace07de73522ccfdd87eecf71ec0563b
김채우 <codn0185@gmail.com>
2026-08-25 12:14:20 +0900

    fix: 플레이어 오버랩 시 활성화하도록 수정

--- Source/SpartaCH3Assignment3/Private/Items/BaseItem.cpp
+++ Source/SpartaCH3Assignment3/Private/Items/BaseItem.cpp
@@ -21,6 +21,10 @@ ABaseItem::ABaseItem()
 
 void ABaseItem::OnBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	if (IsValid(OtherActor) && OtherActor->ActorHasTag("Player"))
+	{
+		Activate(OtherActor);
+	}
 }
 
 void ABaseItem::OnEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
