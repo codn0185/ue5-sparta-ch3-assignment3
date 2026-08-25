32005a8d223749649cb22d8a7547724c68be6ad3
김채우 <codn0185@gmail.com>
2026-08-25 11:40:09 +0900

    feat: FItemSpawnRow 구조체 생성
    - 아이템 소환에 사용될 데이터테이블의 행
    - 이름, 클래스, 확률 정보 저장

---
+++ Source/SpartaCH3Assignment3/Public/Data/ItemSpawnRow.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "ItemSpawnRow.generated.h"
+
+class ABaseItem;
+
+USTRUCT(BlueprintType)
+struct FItemSpawnRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+  public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName ItemName;  // 아이템 이름
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<ABaseItem> ItemClass;  // 아이템 클래스
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SpawnChance;  // 소환 확률
+};
