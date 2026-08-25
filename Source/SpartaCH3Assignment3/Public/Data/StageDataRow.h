a5f1babfefe59a40583d1ba319222e8195864b9a
김채우 <codn0185@gmail.com>
2026-08-25 14:26:32 +0900

    refactor: TSoftObjectPtr를 원시 포인터로 수정

--- Source/SpartaCH3Assignment3/Public/Data/StageDataRow.h
+++ Source/SpartaCH3Assignment3/Public/Data/StageDataRow.h
@@ -13,9 +13,9 @@ struct FStageDataRow : public FTableRowBase
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	FName StageName;  // 스테이지 이름
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TSoftObjectPtr<UWorld> Level;  // 스테이지 맵
+	UWorld* Level;  // 스테이지 맵
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float TimeLimit;  // 제한 시간
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TSoftObjectPtr<UDataTable> ItemSpawnTable;  // 아이템 소환 테이블
+	UDataTable* ItemSpawnTable;  // 아이템 소환 테이블
 };
