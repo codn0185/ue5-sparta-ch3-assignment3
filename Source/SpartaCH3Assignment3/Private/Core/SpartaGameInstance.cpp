db147f56f4864d857ef0307ff4d1e25c4c94e385
김채우 <codn0185@gmail.com>
2026-08-26 20:42:40 +0900

    feat: USpartaGameInstance 클래스 생성

---
+++ Source/SpartaCH3Assignment3/Private/Core/SpartaGameInstance.cpp
@@ -0,0 +1,97 @@
+#include "Core/SpartaGameInstance.h"
+
+#include "Data/StageDataRow.h"
+#include "Kismet/GameplayStatics.h"
+
+USpartaGameInstance::USpartaGameInstance()
+	: StageDataTable(nullptr),
+	  CurrentStageIndex(0),
+	  TotalScore(0)
+{
+}
+
+void USpartaGameInstance::StartGame()
+{
+	UE_LOG(LogTemp, Warning, TEXT("USpartaGameInstance::StartGame()"));
+
+	CurrentStageIndex = 0;
+	TotalScore = 0;
+
+	StartStage();
+}
+
+void USpartaGameInstance::StartStage()
+{
+	UE_LOG(LogTemp, Warning, TEXT("USpartaGameInstance::StartStage()"));
+
+	// 현재 스테이지 맵 로드
+	const FStageDataRow* StageData = GetCurrentStageData();
+	if (!StageData || StageData->Level.IsNull())
+	{
+		return;
+	}
+
+	UGameplayStatics::OpenLevelBySoftObjectPtr(this, StageData->Level);
+}
+
+void USpartaGameInstance::OnStageClear()
+{
+	UE_LOG(LogTemp, Warning, TEXT("USpartaGameInstance::OnStageClear()"));
+
+	// 다음 스테이지
+	CurrentStageIndex++;
+
+	// 마지막 스테이지 - 게임 클리어
+	if (!GetCurrentStageData())
+	{
+		OnGameClear();
+		return;
+	}
+
+	// 다음 스테이지 진행
+	StartStage();
+}
+
+void USpartaGameInstance::OnGameOver()
+{
+	// 게임 오버
+	UE_LOG(LogTemp, Warning, TEXT("USpartaGameInstance::OnGameOver()"));
+}
+
+void USpartaGameInstance::OnGameClear()
+{
+	// 게임 클리어
+	UE_LOG(LogTemp, Warning, TEXT("USpartaGameInstance::OnGameClear()"));
+}
+
+void USpartaGameInstance::AddScore(int32 Score)
+{
+	TotalScore += Score;
+}
+
+const FStageDataRow* USpartaGameInstance::GetCurrentStageData() const
+{
+	if (!StageDataTable)
+	{
+		return nullptr;
+	}
+
+	const TArray<FName> RowNames = StageDataTable->GetRowNames();
+
+	if (RowNames.IsValidIndex(CurrentStageIndex))
+	{
+		return StageDataTable->FindRow<FStageDataRow>(RowNames[CurrentStageIndex], TEXT("USpartaGameInstance::GetCurrentStageData"));
+	}
+
+	return nullptr;
+}
+
+int32 USpartaGameInstance::GetCurrentStageIndex() const
+{
+	return CurrentStageIndex;
+}
+
+int32 USpartaGameInstance::GetTotalScore() const
+{
+	return TotalScore;
+}
---
+++ Source/SpartaCH3Assignment3/Public/Core/SpartaGameInstance.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Engine/GameInstance.h"
+
+#include "SpartaGameInstance.generated.h"
+
+struct FStageDataRow;
+
+UCLASS()
+class SPARTACH3ASSIGNMENT3_API USpartaGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+  protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game|Data")
+	TObjectPtr<UDataTable> StageDataTable;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game|Data")
+	int32 CurrentStageIndex;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game|Data")
+	int32 TotalScore;
+
+  public:
+	USpartaGameInstance();
+
+	UFUNCTION(BlueprintCallable, Category = "Game|Flows")
+	void StartGame();
+	UFUNCTION(BlueprintCallable, Category = "Game|Flows")
+	void StartStage();
+
+	UFUNCTION(BlueprintCallable, Category = "Game|Flows")
+	void OnStageClear();
+	UFUNCTION(BlueprintCallable, Category = "Game|Flows")
+	void OnGameOver();
+	UFUNCTION(BlueprintCallable, Category = "Game|Flows")
+	void OnGameClear();
+
+	void AddScore(int32 Score);
+
+	const FStageDataRow* GetCurrentStageData() const;
+	int32 GetCurrentStageIndex() const;
+	int32 GetTotalScore() const;
+};
