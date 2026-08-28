d7facbbfd01280a23911b78d994df3298a063b34
김채우 <codn0185@gmail.com>
2026-08-28 14:58:29 +0900

    refactor: 게임 인스턴스 사용 방식 수정 및 StartItemSpawn() 내부 구현

--- Source/SpartaCH3Assignment3/Private/Core/SpartaGameState.cpp
+++ Source/SpartaCH3Assignment3/Private/Core/SpartaGameState.cpp
@@ -20,6 +20,14 @@ void ASpartaGameState::BeginPlay()
 {
 	Super::BeginPlay();
 
+	// 게임 인스턴스 검증
+	if (!GetGameInstance<USpartaGameInstance>())
+	{
+		UE_LOG(LogTemp, Error, TEXT("ASpartaGameState::BeginPlay() - SpartaGameInstance 로드 실패"));
+		return;
+	}
+
+	// 게임 시작
 	InitializeLevel();
 }
 
@@ -27,63 +35,20 @@ void ASpartaGameState::InitializeLevel()
 {
 	UE_LOG(LogTemp, Warning, TEXT("ASpartaGameState::InitializeLevel()"));
 
-	// GameInstance에서 현재 스테이지의 제한 시간, 소환 개수, 아이템 데이터 테이블 받아오기
-	float StageDuration = 0.f;
-	int32 SpawnItemCount = 0;
-	UDataTable *ItemDataTable = nullptr;
-	if (UGameInstance *GameInstance = GetGameInstance())
-	{
-		if (USpartaGameInstance *SpartaGameInstance = Cast<USpartaGameInstance>(GameInstance))
-		{
-			const FStageDataRow *StageDate = SpartaGameInstance->GetCurrentStageData();
-			StageDuration = StageDate->TimeLimit;
-			SpawnItemCount = StageDate->ItemCount;
-			ItemDataTable = StageDate->ItemSpawnTable;
-		}
-	}
-
 	// GameData 초기화
 	StageScore = 0;
 	SpawnedCoinCount = 0;
 	CollectedCointCount = 0;
 
-	// SpawnVolume 찾기
-	TArray<AActor *> SpawnVolumes;
-	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AItemSpawnVolume::StaticClass(), SpawnVolumes);
-
-	if (SpawnVolumes.IsEmpty())
-	{
-		UE_LOG(LogTemp, Error, TEXT("AMyGameState::BeginPlay - No SpawnVolume In Current Level"));
-		return;
-	}
-
-	AItemSpawnVolume *ItemSpawnVolume = Cast<AItemSpawnVolume>(SpawnVolumes[0]);
-	if (!ItemSpawnVolume)
-	{
-		UE_LOG(LogTemp, Error, TEXT("AMyGameState::BeginPlay - No SpawnVolume In Current Level"));
-		return;
-	}
-
-	// 아이템 소환
-	ItemSpawnVolume->SetItemSpawnTable(ItemDataTable);
-	for (int i = 0; i < SpawnItemCount; i++)
-	{
-		AActor *SpawnedActor = ItemSpawnVolume->SpawnActor();
-		if (SpawnedActor && SpawnedActor->IsA(ACoinItem::StaticClass()))
-		{
-			SpawnedCoinCount++;
-		}
-	}
-
-	// TODO: 코인 아이템 최소 보장 (1개 이상 필수)
-	// 임시 코드
-	if (SpawnedCoinCount <= 0)
-	{
-		UE_LOG(LogTemp, Error, TEXT("AMyGameState::BeginPlay - SpawnedCoinCount is 0"));
-		return;
-	}
+	// 액터 소환
+	StartItemSpawn();
+	StartSpikeTrapSpawn();
+	StartFallingObjectSpawn();
 
 	// 타이머 설정 - 제한 시간 적용
+	const FStageDataRow *StageDate = GetGameInstance<USpartaGameInstance>()->GetCurrentStageData();
+	const float StageDuration = StageDate->TimeLimit;
+
 	GetWorldTimerManager().SetTimer(
 		LevelTimerHandle,
 		this,
@@ -99,14 +64,8 @@ void ASpartaGameState::ClearStage()
 	// 타이머 정리
 	GetWorldTimerManager().ClearTimer(LevelTimerHandle);
 
-	// GameInstance의 OnStageClear() 호출 - 다음 스테이지 진행 or 게임 클리어
-	if (UGameInstance *GameInstance = GetGameInstance())
-	{
-		if (USpartaGameInstance *SpartaGameInstance = Cast<USpartaGameInstance>(GameInstance))
-		{
-			SpartaGameInstance->OnStageClear();
-		}
-	}
+	// GameInstance의 OnStageClear() 호출
+	GetGameInstance<USpartaGameInstance>()->OnStageClear();
 }
 
 void ASpartaGameState::OnTimeExpired()
@@ -152,6 +111,47 @@ void ASpartaGameState::NotifyPlayerDead()
 
 void ASpartaGameState::StartItemSpawn()
 {
+	// GameInstance에서 필요한 정보 로드
+	const FStageDataRow *StageDate = GetGameInstance<USpartaGameInstance>()->GetCurrentStageData();
+	const float StageDuration = StageDate->TimeLimit;
+	const int32 SpawnItemCount = StageDate->ItemCount;
+	UDataTable *ItemDataTable = StageDate->ItemSpawnTable;
+
+	// SpawnVolume 찾기
+	TArray<AActor *> SpawnVolumes;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AItemSpawnVolume::StaticClass(), SpawnVolumes);
+
+	if (SpawnVolumes.IsEmpty())
+	{
+		UE_LOG(LogTemp, Error, TEXT("AMyGameState::BeginPlay - No SpawnVolume In Current Level"));
+		return;
+	}
+
+	AItemSpawnVolume *ItemSpawnVolume = Cast<AItemSpawnVolume>(SpawnVolumes[0]);
+	if (!ItemSpawnVolume)
+	{
+		UE_LOG(LogTemp, Error, TEXT("AMyGameState::BeginPlay - No SpawnVolume In Current Level"));
+		return;
+	}
+
+	// 아이템 소환
+	ItemSpawnVolume->SetItemSpawnTable(ItemDataTable);
+	for (int i = 0; i < SpawnItemCount; i++)
+	{
+		AActor *SpawnedActor = ItemSpawnVolume->SpawnActor();
+		if (SpawnedActor && SpawnedActor->IsA(ACoinItem::StaticClass()))
+		{
+			SpawnedCoinCount++;
+		}
+	}
+
+	// TODO: 코인 아이템 최소 보장 (1개 이상 필수)
+	// 임시 코드
+	if (SpawnedCoinCount <= 0)
+	{
+		UE_LOG(LogTemp, Error, TEXT("AMyGameState::BeginPlay - SpawnedCoinCount is 0"));
+		return;
+	}
 }
 
 void ASpartaGameState::StartSpikeTrapSpawn()
