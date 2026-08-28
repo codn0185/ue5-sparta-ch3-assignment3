775db6ec5a234fb6eb63fe5455d550f0305bbfda
김채우 <codn0185@gmail.com>
2026-08-28 12:09:09 +0900

    feat: FTableRowBase 구조체 생성
    - 각 웨이브의 정보를 담은 데이터테이블의 행으로 사용할 구조체

---
+++ Source/SpartaCH3Assignment3/Public/Data/WaveDataRow.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "WaveDataRow.generated.h"
+
+USTRUCT(BlueprintType)
+struct FWaveDataRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+  public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 WaveNumber;  // 웨이브 번호
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float WaveTime;  // 웨이브 시간 (초)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 ItemCount;  // 아이템 개수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UDataTable> ItemSpawnTable;  // 아이템 소환 테이블
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 SpikeTrapCount;  // 가시 발판 개수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SpikeTrapActiveDuration;  // 가시 발판 활성화 시간 (초)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float SpikeTrapInactiveDuration;  // 가시 발판 비활성화 시간 (초)
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 FallingObstacleCount;  // 낙하 장애물 동시 소환 개수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float FallingObstacleSpawnInterval;  // 낙하 장애물 소환 간격 (초)
+};
