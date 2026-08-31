49293918b7d15eaf0597cac08bbf46fe2e3573e6
김채우 <codn0185@gmail.com>
2026-08-31 14:12:49 +0900

    build: UMG 모듈 추가

--- Source/SpartaCH3Assignment3/SpartaCH3Assignment3.Build.cs
+++ Source/SpartaCH3Assignment3/SpartaCH3Assignment3.Build.cs
@@ -7,14 +7,21 @@ public class SpartaCH3Assignment3 : ModuleRules
 	public SpartaCH3Assignment3(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"UMG",
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
