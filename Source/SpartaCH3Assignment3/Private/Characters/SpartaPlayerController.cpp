6214a8c11024400c841ec34a9b80fe9892c8e602
김채우 <codn0185@gmail.com>
2026-08-31 14:27:36 +0900

    feat: UI 위젯 변수 및 메서드 추가
    - 변수: 위젯 클래스 및 위젯 인스턴스
    - 메서드: 위젯 표시/숨기기

--- Source/SpartaCH3Assignment3/Private/Characters/SpartaPlayerController.cpp
+++ Source/SpartaCH3Assignment3/Private/Characters/SpartaPlayerController.cpp
@@ -2,12 +2,24 @@
 
 #include "EnhancedInputSubsystems.h"
 
+#include "Blueprint/UserWidget.h"
+
 ASpartaPlayerController::ASpartaPlayerController()
-	: CharacterInputMappingContext(nullptr),
+	:  // IMC & IA
+	  CharacterInputMappingContext(nullptr),
 	  MoveAction(nullptr),
 	  LookAction(nullptr),
 	  JumpAction(nullptr),
-	  SprintAction(nullptr)
+	  SprintAction(nullptr),
+	  // UI
+	  MainMenuWidgetClass(nullptr),
+	  MainMenuWidget(nullptr),
+	  GameOverWidgetClass(nullptr),
+	  GameOverWidget(nullptr),
+	  GameClearWidgetClass(nullptr),
+	  GameClearWidget(nullptr),
+	  GameHUDWidgetClass(nullptr),
+	  GameHUDWidget(nullptr)
 {
 }
 
@@ -24,5 +36,121 @@ void ASpartaPlayerController::BeginPlay()
 				Subsystem->AddMappingContext(CharacterInputMappingContext, 0);
 			}
 		}
+	}
+}
+
+void ASpartaPlayerController::ShowMainMenu()
+{
+	// MainMenuWidget 생성
+	if (!MainMenuWidget)
+	{
+		if (MainMenuWidgetClass)
+		{
+			MainMenuWidget = CreateWidget<UUserWidget>(this, MainMenuWidgetClass);
+		}
+	}
+
+	// Viewport에 추가
+	if (MainMenuWidget)
+	{
+		MainMenuWidget->AddToViewport();
+
+		bShowMouseCursor = true;
+		SetInputMode(FInputModeUIOnly());
+	}
+}
+
+void ASpartaPlayerController::HideMainMenu()
+{
+	if (MainMenuWidget)
+	{
+		MainMenuWidget->RemoveFromParent();
+	}
+}
+
+void ASpartaPlayerController::ShowGameOver()
+{
+	// GameOverWidget 생성
+	if (!GameOverWidget)
+	{
+		if (GameOverWidgetClass)
+		{
+			GameOverWidget = CreateWidget<UUserWidget>(this, GameOverWidgetClass);
+		}
+	}
+
+	// Viewport에 추가
+	if (GameOverWidget)
+	{
+		GameOverWidget->AddToViewport();
+
+		bShowMouseCursor = true;
+		SetInputMode(FInputModeUIOnly());
+	}
+}
+
+void ASpartaPlayerController::HideGameOver()
+{
+	if (GameOverWidget)
+	{
+		GameOverWidget->RemoveFromParent();
+	}
+}
+
+void ASpartaPlayerController::ShowGameClear()
+{
+	// GameClearWidget 생성
+	if (!GameClearWidget)
+	{
+		if (GameClearWidgetClass)
+		{
+			GameClearWidget = CreateWidget<UUserWidget>(this, GameClearWidgetClass);
+		}
+	}
+
+	// Viewport에 추가
+	if (GameClearWidget)
+	{
+		GameClearWidget->AddToViewport();
+
+		bShowMouseCursor = true;
+		SetInputMode(FInputModeUIOnly());
+	}
+}
+
+void ASpartaPlayerController::HideGameClear()
+{
+	if (GameClearWidget)
+	{
+		GameClearWidget->RemoveFromParent();
+	}
+}
+
+void ASpartaPlayerController::ShowGameHUD()
+{
+	// GameHUDWidget 생성
+	if (!GameHUDWidget)
+	{
+		if (GameHUDWidgetClass)
+		{
+			GameHUDWidget = CreateWidget<UUserWidget>(this, GameHUDWidgetClass);
+		}
+	}
+
+	// Viewport에 추가
+	if (GameHUDWidget)
+	{
+		GameHUDWidget->AddToViewport();
+
+		bShowMouseCursor = false;
+		SetInputMode(FInputModeGameOnly());
+	}
+}
+
+void ASpartaPlayerController::HideGameHUD()
+{
+	if (GameHUDWidget)
+	{
+		GameHUDWidget->RemoveFromParent();
 	}
 }
--- Source/SpartaCH3Assignment3/Public/Characters/SpartaPlayerController.h
+++ Source/SpartaCH3Assignment3/Public/Characters/SpartaPlayerController.h
@@ -9,6 +9,8 @@
 class UInputMappingContext;
 class UInputAction;
 
+class UUserWidget;
+
 UCLASS()
 class SPARTACH3ASSIGNMENT3_API ASpartaPlayerController : public APlayerController
 {
@@ -18,18 +20,67 @@ class SPARTACH3ASSIGNMENT3_API ASpartaPlayerController : public APlayerControlle
 	ASpartaPlayerController();
 
 	// InputMappingContext & InputAction
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Properties|Input")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inputs|InputMappingContext")
 	TObjectPtr<UInputMappingContext> CharacterInputMappingContext;  // IMC_Character
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Properties|Input")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inputs|InputAction")
 	TObjectPtr<UInputAction> MoveAction;  // IA_Move
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Properties|Input")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inputs|InputAction")
 	TObjectPtr<UInputAction> LookAction;  // IA_Look
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Properties|Input")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inputs|InputAction")
 	TObjectPtr<UInputAction> JumpAction;  // IA_Jump
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Properties|Input")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Inputs|InputAction")
 	TObjectPtr<UInputAction> SprintAction;  // IA_Sprint
 
   protected:
-	// Callback Methods
+	// UI - MainMenu
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI|MainMenu")
+	TSubclassOf<UUserWidget> MainMenuWidgetClass;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "UI|MainMenu")
+	UUserWidget* MainMenuWidget;
+
+	// UI - GameOver
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI|GameOver")
+	TSubclassOf<UUserWidget> GameOverWidgetClass;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "UI|GameOver")
+	UUserWidget* GameOverWidget;
+
+	// UI - GameClear
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI|GameClear")
+	TSubclassOf<UUserWidget> GameClearWidgetClass;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "UI|GameClear")
+	UUserWidget* GameClearWidget;
+
+	// UI - HUD
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI|GameHUD")
+	TSubclassOf<UUserWidget> GameHUDWidgetClass;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "UI|GameHUD")
+	UUserWidget* GameHUDWidget;
+
+  protected:
 	virtual void BeginPlay() override;
+
+  public:
+	// UI - MainMenu
+	UFUNCTION(BlueprintCallable, Category = "UI|MainMenu")
+	void ShowMainMenu();
+	UFUNCTION(BlueprintCallable, Category = "UI|MainMenu")
+	void HideMainMenu();
+
+	// UI - GameOver
+	UFUNCTION(BlueprintCallable, Category = "UI|GameOver")
+	void ShowGameOver();
+	UFUNCTION(BlueprintCallable, Category = "UI|GameOver")
+	void HideGameOver();
+
+	// UI - GameClear
+	UFUNCTION(BlueprintCallable, Category = "UI|GameClear")
+	void ShowGameClear();
+	UFUNCTION(BlueprintCallable, Category = "UI|GameClear")
+	void HideGameClear();
+
+	// UI - HUD
+	UFUNCTION(BlueprintCallable, Category = "UI|GameHUD")
+	void ShowGameHUD();
+	UFUNCTION(BlueprintCallable, Category = "UI|GameHUD")
+	void HideGameHUD();
 };
